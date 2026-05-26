90bb993ff1894009d1fb7ad288458b32bad3dba0
Dave Welsh <dave@qccareerschool.com>
2026-05-26 12:11:35 -0400

    Added second test

--- jest.config.ts
+++ jest.config.ts
@@ -24,6 +24,7 @@ const config: JestConfig = {
     '<rootDir>/jest.env.ts',
   ],
   collectCoverage: true,
+  clearMocks: true,
 };
 
 export default config;
--- src/queries/queries.test.ts
+++ src/queries/queries.test.ts
@@ -17,10 +17,24 @@ describe('query runner module', () => {
   describe('generateResponse', () => {
     it('reattempts the query if the hallucination tripwire is triggered', async () => {
       const outputText = faker.lorem.lines(3);
-      createMock.mockResolvedValueOnce({ output_text: '' });
-      createMock.mockResolvedValueOnce({ output_text: outputText });
+      createMock.mockResolvedValue({ output_text: outputText });
       const failingResult: GuardrailResult[] = [ { info: { guardrail_name: 'Hallucination Detection' }, tripwireTriggered: true } ];
       const passingResult: GuardrailResult[] = [ { info: { guardrail_name: 'Hallucination Detection' }, tripwireTriggered: false } ];
+      runGuardrailsMock.mockResolvedValueOnce(failingResult);
+      runGuardrailsMock.mockResolvedValueOnce(passingResult);
+      const userInput = faker.lorem.lines(4);
+
+      const response = await generateResponse(userInput);
+      expect(createMock).toHaveBeenCalledTimes(2);
+      expect(response).toEqual(outputText);
+    });
+
+    it('reattempts only once', async () => {
+      const outputText = faker.lorem.lines(3);
+      createMock.mockResolvedValue({ output_text: outputText });
+      const failingResult: GuardrailResult[] = [ { info: { guardrail_name: 'Hallucination Detection' }, tripwireTriggered: true } ];
+      const passingResult: GuardrailResult[] = [ { info: { guardrail_name: 'Hallucination Detection' }, tripwireTriggered: false } ];
+      runGuardrailsMock.mockResolvedValueOnce(failingResult);
       runGuardrailsMock.mockResolvedValueOnce(failingResult);
       runGuardrailsMock.mockResolvedValueOnce(passingResult);
       const userInput = faker.lorem.lines(4);
